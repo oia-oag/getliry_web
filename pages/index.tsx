5017fbc6880b581f060a25e2d21d9f3a8ec07cd0
oia-oag <origin@oiaglobal.site>
2025-05-04 21:57:02 -0600

    Fix: Ajuste en importación de logo y despliegue del ChatOnboarding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 
 import Head from 'next/head';
 import Image from 'next/image';
-import logo from '../public/getliry-logo.png';
 import ChatOnboarding from '../components/ChatOnboarding';
 import '../styles/chat.css';
 
@@ -13,7 +12,7 @@ export default function Home() {
         <meta name="description" content="Onboarding poético con Liry" />
       </Head>
       <main className="min-h-screen bg-gradient-to-br from-indigo-50 to-purple-100 flex flex-col items-center justify-center text-center p-4">
-        <Image src={logo} alt="GetLiry Logo" width={128} height={128} />
+        <Image src="/getliry-logo.png" alt="GetLiry Logo" width={128} height={128} />
         <h1 className="text-4xl font-bold mt-4">Bienvenid@ a GetLiry</h1>
         <ChatOnboarding />
       </main>
